adb9620e1aebb648ab1046202232e48f43108e53
Ahmed Salah <ahmed.salah.khodair@gmail.com>
2024-01-25 20:57:08 +0200

    Add tickets due_date_reminder mail spec

--- spec/mailers/tickets_mailer_spec.rb
+++ spec/mailers/tickets_mailer_spec.rb
@@ -2,4 +2,16 @@ require 'rails_helper'
 
 RSpec.describe TicketsMailer, type: :mailer do
 
+  describe 'due_date_reminder' do
+    let(:user_mail) { 'late@user.com' }
+    let(:ticket_title) { 'Sample Title' }
+
+    it 'sends a due date reminder email' do
+      mail = TicketsMailer.due_date_reminder(user_mail, ticket_title)
+      expect(mail.subject).to eq("#{ticket_title} Ticket Due Date Reminder")
+      expect(mail.to).to eq([user_mail])
+      expect(mail.body.encoded).to include('This is a reminder that your ticket is due soon.')
+    end
+  end
+
 end
