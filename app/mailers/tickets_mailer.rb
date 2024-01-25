763b19c539ba824dd6686d125914a99501ecaa54
Ahmed Salah <ahmed.salah.khodair@gmail.com>
2024-01-25 20:55:01 +0200

    Add tickets due_date_reminder mail

--- app/mailers/tickets_mailer.rb
+++ app/mailers/tickets_mailer.rb
@@ -1,3 +1,12 @@
 class TicketsMailer < ApplicationMailer
   
+  def due_date_reminder(user_mail, ticket_title)
+    mail({
+      from: self.class.default[:from],
+      to: user_mail,
+      subject: "#{ticket_title} Ticket Due Date Reminder",
+      body: 'This is a reminder that your ticket is due soon.'
+    })
+  end
+
 end
