95f69d5fe3537263201621adf92bf518e8e3a9a9
Ahmed Salah <ahmed.salah.khodair@gmail.com>
2024-01-26 09:23:46 +0200

    Add users factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,8 +6,8 @@ FactoryBot.define do
   factory :user do
     name { 'Ahmed Salah' }
     mail { generate(:uniqe_mail) }
-    send_due_date_reminder { false }
-    due_date_reminder_interval { 0 }
+    send_due_date_reminder { true }
+    due_date_reminder_interval { 3 }
     due_date_reminder_time { '12:00' }
     time_zone { 'UTC' }
   end
