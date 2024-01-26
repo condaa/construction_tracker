ed344af2e9394bf23c24863c2f9b5a7fd7c42c39
Ahmed Salah <ahmed.salah.khodair@gmail.com>
2024-01-26 09:23:46 +0200

    Add tickets factory

--- spec/factories/tickets.rb
+++ spec/factories/tickets.rb
@@ -2,9 +2,9 @@ FactoryBot.define do
   factory :ticket do
     title { 'Amazing feature' }
     description { 'amazing description' }
-    assigned_user_id { 1 }
-    due_date { Date.today }
+    assigned_user_id { User.first }
+    due_date { Date.today + 3 }
     status_id { Ticket.status_ids[:backlog] }
-    progress { 50 }
+    progress { 90 }
   end
 end
