a9284f0f8e8e60d86cf3c5cf758f2d43c3763337
Ahmed Salah <ahmed.salah.khodair@gmail.com>
2024-01-26 11:21:27 +0200

    Add current_user & per_page methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+
+  # no sessions yet
+  def current_user
+    @current_user ||= User.find(params[:user_id])
+  end
+
+  def per_page
+    10
+  end
+
 end
