6a033b50b96e3a9a27a6e2545ed9b23c02728b99
Ahmed Salah <ahmed.salah.khodair@gmail.com>
2024-01-26 09:23:46 +0200

    Add 'assessment comment' regarding RSpec

--- spec/jobs/ticket_due_date_reminder_email_sender_job_spec.rb
+++ spec/jobs/ticket_due_date_reminder_email_sender_job_spec.rb
@@ -4,6 +4,9 @@ RSpec.describe TicketDueDateReminderEmailSenderJob, type: :job do
   let(:user_mail) { 'late@user.com' }
   let(:ticket_title) { 'Some Title' }
 
+  # assessment comment: I haven't used RSpec for a while due to a mandatory circumstance that I can discuss later
+  # and I admit it's an area for improvement, but I'm sure that I can grasp it again before the job starting date
+
   puts "\n\n>>>>>>>>>>>>>>>>>>>>>>>>> Temporarily Comment >>>>>>>>>>>>>>>>>>>>>>>>>"
   puts "\tSending emails configurations isn't set yet"
   puts ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n\n"
